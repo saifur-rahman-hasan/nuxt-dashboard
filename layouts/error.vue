989868de2a5cd20d279991af4321aa652033e3da
Saifur Rahman <saifur.dohs@gmail.com>
2022-03-18 22:09:08 +0600

    Error Page Design Updated

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,34 @@
 <template>
 	<v-app dark>
-		<h1 v-if="error.statusCode === 404">
-			{{ pageNotFound }}
-		</h1>
-		<h1 v-else>
-			{{ otherError }}
-		</h1>
-		<NuxtLink to="/"> Home page </NuxtLink>
+		<v-container fill-height fluid>
+			<v-row
+				align="center"
+				justify="center">
+				<v-col class="text-center">
+
+					<v-icon size="120" class="mb-10" color="error">mdi-alert-circle</v-icon>
+
+					<v-sheet class="mb-15">
+						<h1 v-if="error.statusCode === 404" class="display-3 font-weight-thin">
+							{{ pageNotFound }}
+						</h1>
+
+						<h1 v-else>
+							{{ otherError }}
+						</h1>
+					</v-sheet>
+
+					<v-sheet class="">
+						<v-btn to="/" color="primary" class="px-10" large>
+							<v-icon class="mr-3">mdi-transfer-left</v-icon>
+							Back to Home page
+						</v-btn>
+					</v-sheet>
+
+
+				</v-col>
+			</v-row>
+		</v-container>
 	</v-app>
 </template>
 
